775104a8240180e98bff2b7f38b30dded07d6697
IonutRazvanCopos <ionut_copos@yahoo.com>
2025-01-03 19:57:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+const startButton = document.getElementById("startBtn");
+const timer = document.getElementById('time');
+const text = document.getElementById('text');
+const resetButton = document.getElementById('resetBtn');
+
+const getRandomText = () => {
+    const { words } = textList[Math.floor(Math.random() * textList.length)];
+    text.innerHTML = words;
+};
+
+let newText = document.getElementById('myText');
+let newTextArray = newText.innerHTML.split('');
+let testFinished = false;
+let seconds = 60, min = 60, oneSec = 1000;
+let index = 0, indexNewText = 0;
+let wrongWords = 0;
+let result = document.createElement('div');
+result.id = 'result';
+document.body.appendChild(result);
+
+timer.innerHTML = '01:00';
+
+getRandomText();
+
+startButton.addEventListener('click', startTypingTest);
+
+function startTypingTest() {
+    startButton.disabled = true;
+    countdown();
+    startButton.style.cursor = 'auto';
+    document.addEventListener('keypress', checkLetters);
+    newText.style.display = 'block';
+}
+
+function countdown() {
+    let minutes = Math.floor(seconds / min);
+    let remainingSeconds = seconds % min;
+    let time;
+
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+
+    timer.innerHTML = `0${minutes}:${remainingSeconds}`;
+
+    if (seconds === 0) {
+        clearInterval(countdown);
+        timer.innerHTML = 'Time is up!';
+        testFinished = true;
+        document.removeEventListener('keypress', checkLetters);
+        checkingWords();
+    } else {
+        --seconds;
+        time = setTimeout(countdown, oneSec);
+    }
+    if (testFinished) {
+        clearTimeout(time);
+        updateWrongWords();
+    }
+}
+
+function checkLetters(e) {
+    let key;
+    let textLetters = text.innerHTML.split('');
+    if (window.event) {
+        key = e.keyCode;
+    }
+    key = String.fromCharCode(key).toLowerCase();
+    if (key === ' ' && textLetters[index] !== ' ') {
+        e.preventDefault();
+        return;
+    }
+    if (key === textLetters[index]) {
+        newText.innerHTML += `${key}`;
+        newTextArray[indexNewText] = `<span style="color: green;">${key}</span>`;
+        newText.innerHTML = newTextArray.join('');
+        ++index;
+    } else {
+        newText.innerHTML += `${key}`;
+        newTextArray[indexNewText] = `<span style="color: red;">${key}</span>`;
+        newText.innerHTML = newTextArray.join('');
+    }
+    ++indexNewText;
+    if (index >= textLetters.length) {
+        testFinished = true;
+        document.removeEventListener('keypress', checkLetters);
+        checkingWords();
+        updateWrongWords();
+    }
+}
+
+function checkingWords() { 
+    let textWords = text.innerHTML.split(' ');
+    let newTextWords = newText.innerText.split(' ');
+    for (let i = 0; i < textWords.length; ++i) {
+        if (textWords[i] !== newTextWords[i]) {
+            ++wrongWords;
+        }
+    }
+}
+
+function updateWrongWords() {
+    if (testFinished) {
+        result.innerHTML = `You have ${wrongWords}/${text.innerHTML.split(' ').length} wrong words`;
+        document.body.appendChild(result);
+        resetButton.style.display = 'flex';
+        resetButton.onclick = () => location.reload();
+    }
+}
---
+++ text-list.js
@@ -0,0 +1,23 @@
+const textList = [
+    {
+        words: "the quick brown fox jumps over the lazy dog"
+    },
+    {
+        words: "the five boxing wizards jump quickly"
+    },
+    {
+        words: "pack my box with five dozen liquor jugs"
+    },
+    {
+        words: "azi am scos cainele la plimbare, a fost frig afara si am simtit ca imi ingheata degetele, asa ca nu am stat foarte mult afara ca sa nu ma imbolnavesc"
+    },
+    {
+        words: "maine vreau sa gatesc ceva foarte bun si chiar sper sa-mi iasa. daca nu, o sa comand ceva"
+    },
+    {
+        words: "as vrea sa merg la mare, dar nu am bani. sunt sigur ca pana la vara o sa strang destui bani ca sa ma pot duce la mare, dar pana atunci o sa stau acasa si o sa ma uit la filme"
+    },
+    {
+        words: "azi am invatat sa fac o aplicatie web cu html, css si javascript. am invatat cum sa fac un cronometru si cum sa fac un test de scriere. am invatat cum sa fac un meniu si cum sa fac un formular de contact"
+    }
+];
